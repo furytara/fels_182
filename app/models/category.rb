545f04639235e2eae3a2a4a8fc1be85fdb8d6d3d
Doanh <doanhhnqt74@gmail.com>
2016-09-06 17:44:38 +0700

    Create category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :words, dependent: :destroy
   has_many :lessons
+  scope :update_desc, -> {order(updated_at: :desc)}
+  validates :name, presence: true, length: {maximum: 140}
 end
