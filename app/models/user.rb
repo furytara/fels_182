57a3886af75aad20fe74b8bbdd9bacd2eb46cdaf
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2016-09-06 17:24:36 +0700

    Finish signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save {email.downcase!}
   has_many :lessons
   has_many :activities
   has_many :active_relationships, class_name: Relationship.name,
@@ -7,4 +8,11 @@ class User < ActiveRecord::Base
     foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 100},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :fullname, presence: true, length: {maximum: 50}
+  validates :password, length: {minimum: 6}
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 20160906070745) do
   add_index "results", ["word_id"], name: "index_results_on_word_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.string   "email"
     t.string   "fullname"
     t.boolean  "gender"
@@ -87,7 +86,6 @@ ActiveRecord::Schema.define(version: 20160906070745) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "words", force: :cascade do |t|
     t.string   "content"
