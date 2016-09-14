b012658d038bd7add3fd8a6d7dd1609f4b18ce60
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2016-09-14 17:53:46 +0700

    75904_finish_lesson

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :word
   has_many :results
   validates :content, presence: true, length: {maximum: 50}
+  scope :find_correct_answer, -> do
+    find_by(is_true: true)
+  end
 end
