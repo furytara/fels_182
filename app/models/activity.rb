42d082503d042a22947ef98d71dc5e3fe905bcd9
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2016-09-16 09:18:45 +0700

    75953_create_activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
+  scope :desc_date, -> {order(created_at: :desc)}
+  validates :user, presence: true
+  enum action_type: [:follow, :unfollow, :create_lesson, :update_lesson]
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,15 @@
 class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: User.name
   belongs_to :followed, class_name: User.name
+
+  private
+  def create_follow_activity
+    Activity.create user_id: follower_id, target_id: followed_id,
+      action_type: :follow
+  end
+
+  def create_unfollow_activity
+    Activity.create user_id: follower_id, target_id: followed_id,
+      action_type: :unfollow
+  end
 end
